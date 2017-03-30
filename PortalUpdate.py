fbd223ac3af0a18607ca2e86be161049657c82e7
mattbitter <bitter.mca@gmail.com>
2017-03-29 22:47:47 -0400

    update

--- PortalUpdate.py
+++ PortalUpdate.py
@@ -1,20 +1,24 @@
 
 # coding: utf-8
 
-# In[66]:
+# In[34]:
 
 import datetime
 import webbrowser
 now = datetime.datetime.now()
 
+import win32api, win32con
+import keyboard
+import time
 
-# In[54]:
 
-file = open("C:\\Users\\Splinta\\GoogleDrive\\GitStuff\\PortalUpdate\\bi_state.txt","r")
+# In[64]:
+
+file = open("\\\camis2-mlf-fp01\\Project Leapfrog\\9. Business Analytics\\NWPortalUpdateFile\\bi_state.txt","r")
 filedata = file.read()
 
 
-# In[56]:
+# In[65]:
 
 # Replace the target string
 filedata = filedata.replace('xDay', now.strftime("%A"))
@@ -23,22 +27,76 @@ filedata = filedata.replace('xNum', str(now.day))
 filedata = filedata.replace('xYear', str(now.year))
 
 
-# In[65]:
+# In[66]:
 
 # Write the file out again
-file = open("C:\\Users\\Splinta\\GoogleDrive\\GitStuff\\PortalUpdate\\u_bi_state.txt", "w")
+file = open("\\\camis2-mlf-fp01\\Project Leapfrog\\9. Business Analytics\\NWPortalUpdateFile\\bi_state.html", "w")
 file.write(filedata)
 file.close()
 
 
-# In[67]:
+# In[18]:
 
-webbrowser.open('http://mlfess.mapleleaf.ca/irj/portal')
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 
-# In[64]:
+# In[75]:
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+webbrowser.get(chrome_path).open('http://caeppap01.mapleleaf.ca:53900/irj/go/km/navigation/documents/MLF/pages?StartUri=/')
+
+time.sleep(2)
+
+keyboard.write('')
+keyboard.press_and_release('tab')
+time.sleep(1)
+keyboard.write('')
+
+time.sleep(1)
+keyboard.press_and_release('enter')
+time.sleep(2)
+
+click(201,75)
+time.sleep(1)
+keyboard.press_and_release('enter')
 
 
+click(201,75)
+time.sleep(1)
+
+click(201,92)
+time.sleep(1)
+click(252,113)
+time.sleep(1)
+
+time.sleep(1)
+click(109,204)
+
+time.sleep(1)
+#click(788,205)
+keyboard.press_and_release('Alt+d')
+time.sleep(1)
+
+text_path = str('\\\camis2-mlf-fp01\\Project Leapfrog\\9. Business Analytics\\NWPortalUpdateFile')
+
+keyboard.write(text_path)
+keyboard.press_and_release('enter')
+
+time.sleep(1)
+keyboard.press_and_release('Alt+n')
+#click(408,632)
+
+keyboard.write('bi_state.html')
+keyboard.press_and_release('enter')
+
+time.sleep(2)
+click(34,264)
+
+time.sleep(1)
+click(20,203)
 
 
 # In[ ]:
